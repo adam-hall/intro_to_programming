c0a37ed13fdd6787c26b65133ad585c2b660b7fa
Adam Hall <adamhall@gmx.co.uk>
2016-08-03 20:51:44 +0000

    new exercises

---
+++ hash.rb
@@ -0,0 +1,13 @@
+# Create a new hash
+movies = {The_Matrix: 1999,
+          Forrest_Gump: 1998,
+          Shawshank_Redemption: 1992,
+          Gone_Girl: 2014,
+          Memento: 2001}
+          
+#Display values
+puts movies[:The_Matrix]
+puts movies[:Forrest_Gump]
+puts movies[:Shawshank_Redemption]
+puts movies[:Gone_Girl]
+puts movies[:Memento]
